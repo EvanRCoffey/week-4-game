429dd4a88548ab147649f06cd1df1815e4718186
molander86 <rogerecoffey@gmail.com>
2016-11-16 16:26:52 -0600

    Done!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,8 +8,6 @@ var enemy = {};
 var enemiesRemaining = 3;
 //Holds combat data string
 var combatData = "";
-//Empty object
-var emptyObject = {};
 
 //Luke Skywalker object, with stats
 var lukeSkywalker = {
@@ -69,36 +67,38 @@ function attack(){
 			if (hero.hp > 0)
 				//...load combat data into variable
 				combatData = "Our hero attacked the enemy for " + (hero.ap - hero.baseAP) + 
-				" HP.  The enemy attacked our hero for " + enemy.cp + " HP.  Our hero's HP is now " + 
-				hero.hp + ", and the enemy's HP is " + enemy.hp + ".";
+				" HP.<br>  The enemy attacked our hero for " + enemy.cp + " HP.<br>  Hero HP after attack: " + hero.hp +
+				".<br>  Enemy HP after attack: " + enemy.hp + ".<br>  Hero AP after attack: " + hero.ap + ".";
 				//...update textarea with combat data
 				$("#textArea").html(combatData)
 			//If hero is dead...
 			if (hero.hp <= 0) {
-				//...only the restart button will work
+				//...clicking things will not do anything until stage is reset
 				stage = 4;
-				//update textarea with defeat message
-				$("#textArea").html("Our hero was defeated!  The force was not strong with this one.  Game restarted.")
-				//REVEAL RESTART BUTTON
+				//...update textarea with defeat message
+				$("#textArea").html("Our hero was defeated!  The force was not strong with this one.  Game restarted. <br>")
+				//...restart game
 				restart()
 			}
 		}
 		//If enemy is dead...
 		if (enemy.hp <= 0) {
+			//..remove enemy box from the screen
+			hideEnemy();
 			//...clear enemy object
 			enemy = {};
-			//REMOVE DEFEATED CHARACTER FROM SCREEN
+			//remove defeated character from screen
 			if (enemiesRemaining === 0) {
-				//...only the restart button will work
+				//...clicking things will not do anything until stage is reset
 				stage = 4;
 				//...update textarea with game over message
-				$("#textArea").html("Our hero triumphs!  Game over.  Game restarted.")
-				//REVEAL RESTART BUTTON
+				$("#textArea").html("Our hero triumphs!  Game over.  Game restarted.  Select a hero to play again.")
+				//...restart game
 				restart()
 			}
 			//If enemies remain...
 			else {
-				//update textarea with victory message
+				//...update textarea with victory message
 				$("#textArea").html("Our hero defeated the enemy!  Select another enemy.")
 				//...wait for user to select another enemy
 				stage = 2;
@@ -107,29 +107,43 @@ function attack(){
 	}
 }
 
-//This function is called when "restart" button is clicked
+//This function is called when the hero dies or when all three enemies are defeated
 function restart() {
-	//Move all 4 characters to "select your hero" section
-	$("#LS").appendTo("#selectYourHero")
-	$("#OW").appendTo("#selectYourHero")
-	$("#DS").appendTo("#selectYourHero")
-	$("#DM").appendTo("#selectYourHero")
 	stage = 1;
 	enemiesRemaining = 3;
 	hero = {};
 	enemy = {};
+	//Show, move, and reset character objects
 	lukeSkywalker = {hp:100, baseAP:12, cp:5, ap:12};
+	$("#LS").css("display", "inline-block");
+	$("#LS").appendTo("#selectYourHero");
 	obiWan = {hp:120, baseAP:8, cp:10, ap:8};
+	$("#OW").css("display", "inline-block");
+	$("#OW").appendTo("#selectYourHero");
 	darthSidious = {hp:150, baseAP:6, cp:20, ap:6};
+	$("#DS").css("display", "inline-block");
+	$("#DS").appendTo("#selectYourHero");
 	darthMaul = {hp:180, baseAP:5, cp:25, ap:5};
+	$("#DM").css("display", "inline-block");
+	$("#DM").appendTo("#selectYourHero");
 }
 
+//This function is called any time an enemy is defeated.  It hides that enemy from the screen.
+function hideEnemy() {
+	if (enemy.baseAP === 12) $("#LS").css("display", "none")
+	if (enemy.baseAP === 8) $("#OW").css("display", "none")
+	if (enemy.baseAP === 6) $("#DS").css("display", "none")
+	if (enemy.baseAP === 5) $("#DM").css("display", "none")
+}
+
+//This function determines what happens when Luke's box is clicked
 function selectLuke() {
 	if (stage === 1) {
 		hero = lukeSkywalker;
-		//move character box to "yourCharacter"
+		//move hero to "yourCharacter"
 		$("#LS").appendTo("#yourCharacter")
 		stage = 2;
+		$("#textArea").html("Select an enemy!")
 		//move other 3 character boxes to "enemiesAvailable"
 		$("#OW").appendTo("#enemiesAvailable")
 		$("#DS").appendTo("#enemiesAvailable")
@@ -137,19 +151,22 @@ function selectLuke() {
 	}
 	else if (stage === 2) {
 		enemy = lukeSkywalker;
-		//move character box to "fightSection"
+		//move enemy to "fightSection"
 		$("#LS").appendTo("#fightSection")
+		$("#textArea").html("Click the attack button to have your hero attack the enemy!")
 		stage = 3;
 		//Decrement "enemies remaining"
 		enemiesRemaining--;
 	}
 }
 
+//This function determines what happens when Obi-Wan's box is clicked
 function selectObiWan() {
 	if (stage === 1) {
 		hero = obiWan;
-		//move character box to "yourCharacter"
+		//move hero to "yourCharacter"
 		$("#OW").appendTo("#yourCharacter")
+		$("#textArea").html("Select an enemy!")
 		stage = 2;
 		//move other 3 character boxes to "enemiesAvailable"
 		$("#LS").appendTo("#enemiesAvailable")
@@ -158,19 +175,22 @@ function selectObiWan() {
 	}
 	else if (stage === 2) {
 		enemy = obiWan;
-		//move character box to "fightSection"
+		//move enemy to "fightSection"
 		$("#OW").appendTo("#fightSection")
+		$("#textArea").html("Click the attack button to have your hero attack the enemy!")
 		stage = 3;
 		//Decrement "enemies remaining"
 		enemiesRemaining--;
 	}
 }
 
+//This function determines what happens when Darth Sidious's box is clicked
 function selectDarthSidious() {
 	if (stage === 1) {
 		hero = darthSidious;
-		//move character box to "yourCharacter"
+		//move hero to "yourCharacter"
 		$("#DS").appendTo("#yourCharacter")
+		$("#textArea").html("Select an enemy!")
 		stage = 2;
 		//move other 3 character boxes to "enemiesAvailable"
 		$("#LS").appendTo("#enemiesAvailable")
@@ -179,19 +199,22 @@ function selectDarthSidious() {
 	}
 	else if (stage === 2) {
 		enemy = darthSidious;
-		//move character box TO "fightSection"
+		//move enemy to "fightSection"
 		$("#DS").appendTo("#fightSection")
+		$("#textArea").html("Click the attack button to have your hero attack the enemy!")
 		stage = 3;
 		//Decrement "enemies remaining"
 		enemiesRemaining--;
 	}
 }
 
+//This function determines what happens when Darth Maul's box is clicked
 function selectDarthMaul() {
 	if (stage === 1) {
 		hero = darthMaul;
-		//move character box to "yourCharacter"
+		//move hero to "yourCharacter"
 		$("#DM").appendTo("#yourCharacter")
+		$("#textArea").html("Select an enemy!")
 		stage = 2;
 		//move other 3 character boxes to "enemiesAvailable"
 		$("#LS").appendTo("#enemiesAvailable")
@@ -200,8 +223,9 @@ function selectDarthMaul() {
 	}
 	else if (stage === 2) {
 		enemy = darthMaul;
-		//move character box to "fightSection"
+		//move enemy to "fightSection"
 		$("#DM").appendTo("#fightSection")
+		$("#textArea").html("Click the attack button to have your hero attack the enemy!")
 		stage = 3;
 		//Decrement "enemies remaining"
 		enemiesRemaining--;
